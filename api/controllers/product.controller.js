b9901f56230176b7d14bc1f46644335694fe4307
djcesargarcia <djcesargarcia@gmail.com>
2023-05-18 09:50:59 +0100

    Product controller finished OK

--- api/controllers/product.controller.js
+++ api/controllers/product.controller.js
@@ -59,7 +59,21 @@ async function updateOneProduct(req, res){
 }
 
 async function deleteProduct(req, res) {
-
+    try {
+        const product = await Product.destroy({
+            where:{
+                id: req.params.id
+            }
+        }) 
+        if(product)
+        {
+            return res.status(200).json('Product deleted')
+        }else {
+            return res.status(400).send('Product not found')
+        }
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
 }
 
 module.exports = {
