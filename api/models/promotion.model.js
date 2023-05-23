2262707ad240609d07a0360acaed16afe9263fb3
djcesargarcia <djcesargarcia@gmail.com>
2023-05-23 09:08:47 +0100

    Add changes to promotion model Ok

--- api/models/promotion.model.js
+++ api/models/promotion.model.js
@@ -1,21 +1,36 @@
 const { Sequelize } = require ('sequelize')
 const sequelize = new Sequelize('mysql::memory')
 
-const Promotion = sequelize.define('promotion', {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    description: {
-        type:DataTypes.STRING,
-        allowNull: false,
-    },
-    discount: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-})
-
-(async () => {
-    await sequelize.sync({force: true});
+const Promotion = sequelize.define("promotion", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  code: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  photo: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  begin_date: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  finish_date: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  discount: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  
+})(async () => {
+  await sequelize.sync({ force: true });
 })();
